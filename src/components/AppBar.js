ed42a4d9439cb8b815fd0eeb72a2402b766d6f22
Matt Wood <m.wood0904@gmail.com>
2017-11-27 18:08:08 -0600

    fixed router and added user auth

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class AppBar extends React.Component {
   render() {
@@ -7,15 +12,15 @@ class AppBar extends React.Component {
         <div className="nav-wrapper">
           <a id="logo-container" href="/" className="brand-logo">Meal Prep Sunday</a>
           <ul className="right hide-on-med-and-down">
-            <li><a href="/users/logout">Log Out</a></li>
-            <li><a href="/users/register">Sign Up</a></li>
-            <li><a href="/users/login">Login</a></li>
+            <li><a href="/logout">Log Out</a></li>
+            <li><a href="/register">Sign Up</a></li>
+            <li><Link to="/login">Login</Link></li>
           </ul>
           <ul id="nav-mobile" className="side-nav center-align">
             <li><a href="#!">User Email</a></li>
-            <li><a href="/users/logout">Log Out</a></li>
-            <li><a href="/users/register">Sign Up</a></li>
-            <li><a href="/users/login">Login</a></li>
+            <li><a href="/logout">Log Out</a></li>
+            <li><a href="/register">Sign Up</a></li>
+            <li><a href="/login">Login</a></li>
           </ul>
           <a data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
@@ -14,16 +14,37 @@ import LoginPage from './components/LoginPage';
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
-const Root = () => {
-  return (
-    <Router basename="/meal-prep-sunday/">
-      <div>
-        {/* Path to main app page */}
-        <Route exact path='/' component={App} />
-        <Route exact path='/login' component={LoginPage} />
-      </div>
-    </Router>
-  )
+class Root extends Component {
+  constructor() {
+    super();
+
+    this.loginUser = this.loginUser.bind(this);
+  }
+
+  state = {
+    user: null
+  }
+
+  loginUser(user) {
+    console.log(user);
+
+    this.setState({ user })
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          {/* Path to main app page */}
+          <Route exact path='/' component={App} />
+          {/* <Route exact path='/login' loginUser={loginUser} component={LoginPage} /> */}
+          <Route exact path='/login' render={(props) => (
+            <LoginPage loginUser={this.loginUser} />
+          )}/>
+        </div>
+      </Router>
+    )
+  }
 }
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
